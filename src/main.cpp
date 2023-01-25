6e14ad8561e13780cd98e560b1a081f1b2b7e45d
Sebastian <smartsege@protonmail.com>
2023-01-25 22:08:04 +0100

    feature(wifi): Connect to wifi and run program
    See description of previous commit.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,57 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
-void setup() {
-  // put your setup code here, to run once:
+// My own files
+#include "wifi_connect.h"
+
+#define ledPin 14
+#define buttonPin 32
+#define wifiButtonPin 33
+#define wifiConnectPin 27
+
+bool buttonReading;
+bool buttonState;
+bool ledState;
+bool lastButtonState = false;
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLDOWN);
+  pinMode(wifiButtonPin, INPUT_PULLDOWN);
+  pinMode(wifiConnectPin, OUTPUT);
+
+  Serial.begin(9800);
+  connectToWiFi(wifiConnectPin);
+  Serial.println("Setup done.");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  if (digitalRead(wifiButtonPin) == HIGH)
+  {
+
+    connectToWiFi(wifiConnectPin);
+  }
+
+  buttonReading = digitalRead(buttonPin);
+  if (buttonReading != lastButtonState)
+  {
+    lastDebounceTime = millis();
+  }
+  lastButtonState = buttonReading;
+  if ((millis() - lastDebounceTime) > debounceDelay)
+  {
+    if (buttonReading != buttonState)
+    {
+      buttonState = buttonReading;
+      if (buttonState == HIGH)
+      {
+        ledState = !ledState;
+      }
+    }
+  }
+  digitalWrite(ledPin, ledState);
 }
